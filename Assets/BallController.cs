76559c495dcfdc89622c5861f85b02a037ecd09f
Tatyumi <s150204@nishitech.ac.jp>
2019-02-20 16:25:18 +0900

    変数のアクセス修飾子を一部変更
    オブジェクト取得の際にFind関数を用いていたが，
    処理の負担が大きいため，publicに変更し，
    インスペクター上で取得するように変更．
    また，publicに変更したことにより，変数名も修正した．

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -3,22 +3,20 @@ using System.Collections;
 
 public class BallController : MonoBehaviour
 {
-    /// <summary></summary>
-    public float xR;
-    /// <summary></summary>
-    public float yR;
+    /// <summary>ボールの縦軸の角度</summary>
+    public float BallXRange;
+    /// <summary>ボールの横軸の角度</summary>
+    public float BallYRange;
     /// <summary>ボール生成オブジェクト</summary>
-    GameObject generator;
-    /// <summary>開始ボタン</summary>
-    GameObject startButton;
+    public GameObject PlayGenerator;
+    /// <summary>スタートパネル</summary>
+    public GameObject StartPanel;
 
     //GameObject generator = GameObject.Find("PlayGenerator");
     void Start()
     {
-        this.generator = GameObject.Find("PlayGenerator");
-        this.startButton = GameObject.Find("StartPanel");
-
-        this.startButton.transform.localPosition = new Vector3(0, 0, 0);
+        //スタートパネルのの初期位置
+        this.StartPanel.transform.localPosition = new Vector3(0, 0, 0);
     }
 
     /// <summary>
@@ -29,28 +27,28 @@ public class BallController : MonoBehaviour
         //ボール初期位置に配置
         this.transform.localPosition = new Vector3(0, 0, 0);
         //ボールの移動方向
-        xR = Random.Range(-8, 8);
-        yR = Random.Range(-8, 8);
+        BallXRange = Random.Range(-8, 8);
+        BallYRange = Random.Range(-8, 8);
         
-        if (-2 < xR && xR < 2)
+        if (-2 < BallXRange && BallXRange < 2)
         {
             GameStart();
         }
-        else if (-2 < xR && xR < 2)
+        else if (-2 < BallXRange && BallXRange < 2)
         {
             GameStart();
         }
-        this.startButton.transform.localPosition = new Vector3(0, 450, 0);
+        this.StartPanel.transform.localPosition = new Vector3(0, 450, 0);
     }
 
     void Update()
     {
-        this.transform.Translate(xR, yR, 0);
+        this.transform.Translate(BallXRange, BallYRange, 0);
 
         //Y方向の移動量が0の場合
-        if (yR == 0)
+        if (BallYRange == 0)
         {
-            yR += 3;
+            BallYRange += 3;
         }
     }
 
@@ -64,41 +62,41 @@ public class BallController : MonoBehaviour
         if (other.gameObject.tag == "Player1")
         {
             //x軸に方向の反転
-            xR = xR * -1 + 2.0f;
+            BallXRange = BallXRange * -1 + 2.0f;
             if (this.transform.localPosition.y > 0)
             {
-                yR += 1;
+                BallYRange += 1;
             }
             else if (this.transform.localPosition.y < 0)
             {
-                yR -= 1;
+                BallYRange -= 1;
             }
         }
         else if (other.gameObject.tag == "Player2")
         {
             //x軸に方向の反転
-            xR = xR * -1 - 2.0f;
+            BallXRange = BallXRange * -1 - 2.0f;
             if (this.transform.localPosition.y > 0)
             {
-                yR += 1;
+                BallYRange += 1;
             }
             else if (this.transform.localPosition.y < 0)
             {
-                yR -= 1;
+                BallYRange -= 1;
             }
         }
         else if (other.gameObject.tag == "Wall")
         {//y軸の方向の反転
-            yR *= -1;
+            BallYRange *= -1;
         }
         else if (other.gameObject.tag == "Goal1")
         {
-            generator.GetComponent<PlayGenerator>().addScore_p2();
+            PlayGenerator.GetComponent<PlayGenerator>().AddScore_p2();
             GameStart();
         }
         else if (other.gameObject.tag == "Goal2")
         {
-            generator.GetComponent<PlayGenerator>().addScore_p1();
+            PlayGenerator.GetComponent<PlayGenerator>().AddScore_p1();
             GameStart();
         }
     }
