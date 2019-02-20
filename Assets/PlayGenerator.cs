755ca30f3c7c4920cf275fb288cb159bb0df993c
Tatyumi <s150204@nishitech.ac.jp>
2019-02-20 16:15:25 +0900

    変数のアクセス修飾子を一部変更
    オブジェクト取得の際にFind関数を用いると
    処理の付加が大きいため，publicに修正し，
    インスペクター上で取得を行うように変更した．

--- Assets/PlayGenerator.cs
+++ Assets/PlayGenerator.cs
@@ -4,53 +4,52 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class PlayGenerator : MonoBehaviour {
-
-
-    GameObject playerText1;
-    GameObject playerText2;
-    GameObject judagePanel;
-    GameObject againPanel;
-    GameObject ball;
+    
+    /// <summary>プレイヤー1テキスト</summary>
+    public GameObject PlayerText1;
+    /// <summary>プレイヤー2テキスト</summary>
+    public GameObject PlayerText2;
+    /// <summary>ジャッジパネル</summary>
+    public GameObject JudagePanel;
+    /// <summary>アゲインパネル</summary>
+    public GameObject AgainPanel;
+    /// <summary>ボール</summary>
+    public GameObject Ball;
     /// <summary>プレイヤー1のスコア</summary>
     public int score1;
     /// <summary>プレイヤー2のスコア</summary>
     public int score2;
     /// <summary>目標スコア</summary>
-    public int mNum;
+    public int MatchPoint;
 
     // Use this for initialization
     void Start () {
-        this.playerText1 = GameObject.Find("Player1Score");
-        this.playerText2 = GameObject.Find("Player2Score");
-        this.judagePanel = GameObject.Find("JudagePanel");
-        this.againPanel = GameObject.Find("AgainPanel");
-        this.ball = GameObject.Find("Ball");
 
-        this.playerText1.GetComponent<Text>().text = "1P Score:" + score1.ToString();
-        this.playerText2.GetComponent<Text>().text = "2P Score:" + score2.ToString();
+        this.PlayerText1.GetComponent<Text>().text = "1P Score:" + score1.ToString();
+        this.PlayerText2.GetComponent<Text>().text = "2P Score:" + score2.ToString();
         score1 = 0;
         score2 = 0;
-        mNum = TitleDirector.matchNum;
+        MatchPoint = TitleDirector.MatchNum;
     }
 	
     /// <summary>
     /// プレイヤー１のスコア加算
     /// </summary>
-	public void addScore_p1()
+	public void AddScore_p1()
     {
         score1 += 1;
-        this.playerText1.GetComponent<Text>().text = "1P Score:" + score1.ToString();
-        judage(score1,score2);
+        this.PlayerText1.GetComponent<Text>().text = "1P Score:" + score1.ToString();
+        CheckScore(score1,score2);
     }
 
     /// <summary>
     /// プレイヤー2のスコア加算
     /// </summary>
-    public void addScore_p2()
+    public void AddScore_p2()
     {
         score2 += 1;
-        this.playerText2.GetComponent<Text>().text = "2P Score:" + score2.ToString();
-        judage(score1,score2);
+        this.PlayerText2.GetComponent<Text>().text = "2P Score:" + score2.ToString();
+        CheckScore(score1,score2);
     }
 
     /// <summary>
@@ -58,36 +57,36 @@ public class PlayGenerator : MonoBehaviour {
     /// </summary>
     /// <param name="score1"></param>
     /// <param name="score2"></param>
-    public void judage(int score1,int score2)
+    public void CheckScore(int score1,int score2)
     {
         // プレーヤー1が勝利した場合
-        if (score1 == mNum)
+        if (score1 == MatchPoint)
         {
-            this.judagePanel.transform.localPosition = new Vector3(0, 0, 0);
-            this.judagePanel.transform.Rotate(0, 0, 180);
-            Destroy(this.ball);
+            this.JudagePanel.transform.localPosition = new Vector3(0, 0, 0);
+            this.JudagePanel.transform.Rotate(0, 0, 180);
+            Destroy(this.Ball);
         }
-        else if (score2 == mNum)
+        else if (score2 == MatchPoint)
         {
-            this.judagePanel.transform.localPosition = new Vector3(0, 0, 0);
-            Destroy(this.ball);
+            this.JudagePanel.transform.localPosition = new Vector3(0, 0, 0);
+            Destroy(this.Ball);
         }
     }
     
     /// <summary>
     /// コンテニューパネルの表示
     /// </summary>
-    public void againMove() {
+    public void CountinuePanelDisplay() {
 
-        this.againPanel.transform.localPosition = new Vector3(0, 0, 0);
+        this.AgainPanel.transform.localPosition = new Vector3(0, 0, 0);
 
-        Destroy(this.judagePanel);
+        Destroy(this.JudagePanel);
     }
     
     /// <summary>
     /// タイトルシーンに遷移
     /// </summary>
-    public void titleMove()
+    public void TitleSceneMove()
     {
         SceneManager.LoadScene("TitleScene");
     }
@@ -95,7 +94,7 @@ public class PlayGenerator : MonoBehaviour {
     /// <summary>
     /// VSシーンに遷移
     /// </summary>
-    public void playAgain()
+    public void VsSceneMove()
     {
         SceneManager.LoadScene("VsScene");
     }
