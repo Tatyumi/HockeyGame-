3ae2ee4a301bf58fa48d3a7146aee7ad54a5134c
Tatyumi <s150204@nishitech.ac.jp>
2019-02-24 05:17:01 +0900

    新規追加
    シングルトンでオーディオを管理するクラスを追加.

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// オーディオ管理クラス．シングルトン．
+public class AudioManager : SingletonMonoBehaviour<AudioManager>
+{
+    AudioSource audioSource;
+    /// <summary>タイトルシーンのBGM</summary>
+    public AudioClip TitleSceneBGM;
+    /// <summary>プレイシーンのBGM</summary>
+    public AudioClip VsSceneBGM;
+    /// <summary>ボール反射SE</summary>
+    public AudioClip BounceBallSE;
+    /// <summary>ゲーム開始SE</summary>
+    public AudioClip StartGameSE;
+    /// <summary>ゲーム終了SE</summary>
+    public AudioClip EndGameSE;
+    /// <summary>全オウディオ保持ディクショナリ</summary>
+    private Dictionary<string, AudioClip> AudioDic;
+
+    private void Awake()
+    {
+        // 存在確認
+        if (this != Instance)
+        {
+            Destroy(this);
+            return;
+        }
+
+        //シーンが遷移しても破棄されない
+        DontDestroyOnLoad(gameObject);
+
+        audioSource = gameObject.GetComponent<AudioSource>();
+
+        // オウディオを格納
+        AudioDic = new Dictionary<string, AudioClip> {
+            { TitleSceneBGM.name, TitleSceneBGM},
+            { VsSceneBGM.name, VsSceneBGM },
+            { BounceBallSE.name, BounceBallSE },
+            { StartGameSE.name, StartGameSE },
+            { EndGameSE.name, EndGameSE },
+        };
+    }
+    
+
+    /// <summary>
+    /// 音を流す
+    /// </summary>
+    /// <param name="soundName">音の名前</param>
+    public void PlaySound(string soundName)
+    {
+        // 名前のチェック
+        if (!AudioDic.ContainsKey(soundName))
+        {
+            Debug.Log(soundName + "という名前のSEがありません");
+            return;
+        }
+        audioSource.PlayOneShot(AudioDic[soundName]);
+    }
+
+    /// <summary>
+    /// 音を停止
+    /// </summary>
+    public void StopSound()
+    {
+        audioSource.Stop();
+    }
+    
+}
