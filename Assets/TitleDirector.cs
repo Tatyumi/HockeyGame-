1165d7f74399efa4846fa3972ac0db36e3bab166
Tatyumi <s150204@nishitech.ac.jp>
2019-02-24 05:56:23 +0900

    BGM停止処理を追加
    シーン遷移時にBGMを停止する処理を追加

--- Assets/TitleDirector.cs
+++ Assets/TitleDirector.cs
@@ -1,17 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using Common;
 
 public class TitleDirector : MonoBehaviour {
 
-    /// <summary>目標スコア</summary>
-    public static int MatchNum = 0;
+    /// <summary>目標ポイント</summary>
+    public static int MatchPoint = 0;
     /// <summary>セッティングパネル</summary>
     public GameObject SettingPanel;
+    /// <summary>オーディオマネージャー</summary>
+    private AudioManager audioManager;
+
+    private void Awake()
+    {
+        audioManager = GameObject.Find(Constans.AUDIO_MANAGER).GetComponent<AudioManager>();
+    }
 
     void Start()
     {
         DontDestroyOnLoad(this);
+        audioManager.PlaySound(Constans.TITLE_SCENE_BGM);
     }
     
     /// <summary>
@@ -19,8 +28,9 @@ public class TitleDirector : MonoBehaviour {
     /// </summary>
     /// <param name="selectedPoint">選択した目標ポイント</param>
     public void StartProcess(int selectedPoint) {
-        MatchNum = selectedPoint;
-        SceneManager.LoadScene("VsScene");
+        MatchPoint = selectedPoint;
+        SceneManager.LoadScene(Constans.VS_SCENE_NAME);
+        audioManager.StopSound();
     }
 
 
