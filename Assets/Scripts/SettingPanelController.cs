29a60d0e23f18d6a26ce3be73721101710967ce0
Tatyumi <s150204@nishitech.ac.jp>
2019-05-18 23:59:49 +0900

    新規追加
    目標ポイント設定クラスを追加。

---
+++ Assets/Scripts/SettingPanelController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingPanelController : MonoBehaviour {
+    /// <summary>目標ポイント</summary>
+    public static int MatchPoint = 0;
+
+    /// <summary>
+    /// 目標ポイントの設定
+    /// </summary>
+    /// <param name="selectedPoint">選択した目標ポイント</param>
+    public void SetMatchPoint(int selectedPoint)
+    {
+        // 目標ポイントの取得
+        MatchPoint = selectedPoint;
+
+    }
+}
